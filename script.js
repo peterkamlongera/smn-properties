9d7177df8b9c8e2e731718c80e14a9f9760fe7ff
Peter <peterkamlongera@gmail.com>
2022-12-30 19:09:40 +0200

    Added contact form w/captcha, needs testing

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ $toggleHome.addEventListener('click', toggle_checkbox);
 $toggleAbout.addEventListener('click', toggle_checkbox);
 $toggleImpact.addEventListener('click', toggle_checkbox);
 $toggleContact.addEventListener('click', toggle_checkbox);
+
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let yourname = id("name"),
+  email = id("email"),
+  number = id("number"),
+  message = id("message"),
+  form = id("form"),
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  engine(yourname, 0, "Name cannot be blank");
+  engine(email, 1, "Email cannot be blank");
+  engine(number, 2, "Number cannot be blank");
+  engine(message, 3, "Message cannot be blank");
+});
+
+let engine = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    id.style.border = "2px solid red";
+
+  } else {
+    errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid green";
+
+  }
+};
